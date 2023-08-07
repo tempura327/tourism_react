d2df04e17ff2702b08bfd3363e5911505ece69da
tempura327 <cks6658324@yahoo.com.tw>
2023-08-07 22:40:33 +0800

    feat: set MUI  theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import { ThemeProvider } from '@mui/material/styles';
+
+import theme from './theme';
+
 // import router from './router/index';
 import Home from '../src/pages/Home';
 import Activity from '../src/pages/Activity';
 import Attraction from '../src/pages/Attraction';
 
-function App() {
+const App = () => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/tourism_react' element={<Home />}></Route>
-				<Route path='/tourism_react/activity' element={<Activity />}></Route>
-				<Route path='/tourism_react/attraction' element={<Attraction />}></Route>
-			</Routes>
-		</BrowserRouter>
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/tourism_react' element={<Home />}></Route>
+					<Route path='/tourism_react/activity' element={<Activity />}></Route>
+					<Route path='/tourism_react/attraction' element={<Attraction />}></Route>
+				</Routes>
+			</BrowserRouter>
+		</ThemeProvider>
 	);
-}
+};
 
 export default App;
--- src/theme.ts
+++ src/theme.ts
@@ -1,45 +1,79 @@
 import { red, yellow, lightGreen } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
+const palette = {
+	primary: {
+		main: '#08A6BB',
+	},
+	secondary: {
+		main: yellow[600],
+	},
+	legibility: {
+		'80': '#313131',
+		'60': '#525151',
+		'40': '#AEAEAE',
+		'20': '#CBCBCB',
+		white: '#fffff',
+	},
+	success: {
+		main: lightGreen[600],
+		dark: lightGreen[900],
+		light: lightGreen[300],
+	},
+	error: {
+		main: red[600],
+		dark: red[900],
+		light: red[300],
+	},
+	info: {
+		main: '#546487',
+		dark: '#6F7789',
+		light: '#a7b7db',
+	},
+	shadow: {
+		main: 'rgba(51,51,51,.5)',
+		dark: 'rgba(51,51,51,.7)',
+		light: 'rgba(51,51,51,.3)',
+	},
+};
+
 // A custom theme
 const theme = createTheme({
-	palette: {
-		primary: {
-			main: '#08A6BB',
+	components: {
+		MuiAppBar: {
+			styleOverrides: {
+				root: {
+					backgroundColor: 'white',
+					color: palette.primary.main,
+					display: 'flex',
+					justifyContent: 'space-between',
+					flexDirection: 'row',
+				},
+			},
 		},
-		secondary: {
-			main: yellow[600],
+		MuiButton: {
+			styleOverrides: {
+				root: {
+					textTransform: 'none',
+					color: palette.legibility.white,
+				},
+			},
 		},
-		legibility: {
-			'80': '#313131',
-			'60': '#525151',
-			'40': '#AEAEAE',
-			'20': '#CBCBCB',
-			white: '#fffff',
-		},
-		success: {
-			main: lightGreen[600],
-			dark: lightGreen[900],
-			light: lightGreen[300],
-		},
-		error: {
-			main: red[600],
-			dark: red[900],
-			light: red[300],
-		},
-		info: {
-			main: '#546487',
-			dark: '#6F7789',
-			light: '#a7b7db',
-		},
-		shadow: {
-			main: 'rgba(51,51,51,.5)',
-			dark: 'rgba(51,51,51,.7)',
-			light: 'rgba(51,51,51,.3)',
+		MuiTextField: {
+			defaultProps: {
+				InputLabelProps: {
+					shrink: true,
+				},
+			},
 		},
 	},
+	palette,
+	spacing: 4,
 	zIndex: {
 		appBar: 1000,
+	},
+	shape: {
+		borderRadius: 4,
 	},
 });
 
